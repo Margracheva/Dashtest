09ac328e485d4d7269f27765644d96d88b5bf851
Margracheva <margo.gracheva@mail.ru>
2024-08-30 06:04:42 +0300

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -11,6 +11,7 @@ df = pd.read_excel(data_path)
 
 # Initialize the Dash app
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 # Define custom CSS styles for the dropdown
 app.index_string = '''
